82f3d17eb6da52504ac42aa0d6f56d03c9e6d5f8
helaluddin100 <freelancerhelaluddin@gmail.com>
2022-05-22 02:09:09 +0600

    about us page add

---
+++ pages/about.js
@@ -0,0 +1,200 @@
+import AppLayout from "../component/Layout/Layout";
+function About() {
+  return (
+    <>
+      <div className="about cpy-6">
+        <div className="container">
+          <div className="section-title cpb-7">
+            <p className="sm-title">About me</p>
+            <h2 className="sec-title">About my services</h2>
+            <p className="sec-des">
+              Get more software development information about exchange software
+              wallet software derivate products and ETC
+            </p>
+          </div>
+
+          <div className="about-container">
+            <div class="row-col-2">
+              <div class="col-md-6" data-aos="fade-up" data-aos-duration="1000">
+                <div class="about-us-img">
+                  <img src="assets/img/inner/about-us.png" alt="" />
+                </div>
+              </div>
+              <div class="col-md-6" data-aos="fade-up" data-aos-duration="1500">
+                <div class="about-us-content">
+                  <div class="about-content-inner">
+                    <h2>
+                      Integrate innovation with technology through expertise
+                      product delivery
+                    </h2>
+                    <p>
+                      We help you launch your own NFT collectibles minting
+                      website &amp; marketplace to attract the growing community
+                      of NFT users. NFT marketplaces built by us are
+                      feature-rich, decentralized, and facilitate tokenization
+                      of all kinds of assets, including artwork, gaming cards,
+                      software licenses, digital collectibles, and real-world
+                      assets. We have expertise in NFT token development
+                      services, where you get to earn royalties every time a
+                      token is sold. Opt your non-fungible token development
+                      with us and sell over any platform worldwide without any
+                      intermediate authority. We provide NFT development
+                      services for digital content such as art, music and
+                      videos, domain names, gaming items, investment, land
+                      acquisition, and other physical items that are unique.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="service-industries cpb-6">
+        <div className="container">
+          <div className="section-title cpb-7">
+            <p className="sm-title">Industry</p>
+            <h2 className="sec-title">
+              Industries we serve through our Impeccable Solutions
+            </h2>
+            <p className="sec-des">
+              Maticz serves the Cryptosphere through its highly endurant
+              products offering endless quality of service finding use in a
+              variety of domains.
+            </p>
+          </div>
+
+          <div className="service-industries-container">
+            <div class="row-col-5">
+              <div className="col-md-2">
+                <div className="industries-ca">
+                  <div className="industries-ca-inner">
+                    <div className="icon-area">
+                      <img src="assets/img/design/1.webp" alt="" />
+                    </div>
+                    <div className="industries-name">
+                      <p>Fintech</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="industries-ca">
+                  <div className="industries-ca-inner">
+                    <div className="icon-area">
+                      <img src="assets/img/design/2.webp" alt="" />
+                    </div>
+                    <div className="industries-name">
+                      <p>Education</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="industries-ca">
+                  <div className="industries-ca-inner">
+                    <div className="icon-area">
+                      <img src="assets/img/design/3.webp" alt="" />
+                    </div>
+                    <div className="industries-name">
+                      <p>e-Commerce</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="industries-ca">
+                  <div className="industries-ca-inner">
+                    <div className="icon-area">
+                      <img src="assets/img/design/4.webp" alt="" />
+                    </div>
+                    <div className="industries-name">
+                      <p>Transport</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="industries-ca">
+                  <div className="industries-ca-inner">
+                    <div className="icon-area">
+                      <img src="assets/img/design/5.webp" alt="" />
+                    </div>
+                    <div className="industries-name">
+                      <p>Real Estate</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="industries-ca">
+                  <div className="industries-ca-inner">
+                    <div className="icon-area">
+                      <img src="assets/img/design/6.webp" alt="" />
+                    </div>
+                    <div className="industries-name">
+                      <p>Logistics</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="industries-ca">
+                  <div className="industries-ca-inner">
+                    <div className="icon-area">
+                      <img src="assets/img/design/7.webp" alt="" />
+                    </div>
+                    <div className="industries-name">
+                      <p>Insurance</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="industries-ca">
+                  <div className="industries-ca-inner">
+                    <div className="icon-area">
+                      <img src="assets/img/design/8.webp" alt="" />
+                    </div>
+                    <div className="industries-name">
+                      <p>Healthcare</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="industries-ca">
+                  <div className="industries-ca-inner">
+                    <div className="icon-area">
+                      <img src="assets/img/design/9.webp" alt="" />
+                    </div>
+                    <div className="industries-name">
+                      <p>Digital Marketing</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="industries-ca">
+                  <div className="industries-ca-inner">
+                    <div className="icon-area">
+                      <img src="assets/img/design/10.webp" alt="" />
+                    </div>
+                    <div className="industries-name">
+                      <p>Media & Entertainment</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+About.title = "NFT constractor - About";
+About.layout = AppLayout;
+
+export default About;
