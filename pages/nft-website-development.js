500ae9b470383feed4ac841c3be05750bf04dab9
helaluddin100 <freelancerhelaluddin@gmail.com>
2022-05-23 22:33:20 +0600

    update meta image and des

--- pages/nft-website-development.js
+++ pages/nft-website-development.js
@@ -1750,6 +1750,9 @@ function HiCoin() {
 
 HiCoin.title = "Full Stack Web Development Services";
 HiCoin.layout = AppLayout;
+Home.image = "assets/imgs/hero-img-6.png";
+Home.description =
+  "We provide Non-Fungible Development Services that can help you create NFTs for artworks, digital collectibles, gaming, sports, music, video, content Subscription, etc";
 
 const table = [
   {
